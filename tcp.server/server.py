5b31be97f607427daea54d199d1a57d1c0708ef2
‎ <72667107+websiteuser-cmd@users.noreply.github.com>
2024-07-12 00:29:40 +0100

    first commit

---
+++ tcp.server/server.py
@@ -0,0 +1,32 @@
+# /usr/bin/python3
+
+import socket
+
+serverssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Gets the ip of the host
+host = socket.gethostname()
+port = 444
+
+
+# Binding to socket
+serverssocket.bind(('192.168.0.66', port))
+
+#starting the TCP listener
+serverssocket.listen(3)
+
+#STARTING THE CONNECTION    
+while True:
+    clientsocket, address = serverssocket.accept()
+
+    print("Connection Established from: %s " % str(address))
+        
+    message = 'Hello, thankyou for connecting to the server' + "\r\n"
+    
+    clientsocket.send(message.encode('ascii'))
+
+    
+
+    clientsocket.close()
+    
+    
